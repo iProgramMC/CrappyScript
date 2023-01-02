7564e2899088ffda9803e8079609d4c84eccf9cc
iProgramInCpp <ipmcmcpe729@gmail.com>
2023-01-03 00:49:46 +0200

    * Remove some leftover crap.

--- nsshell/nanoshell.c
+++ nsshell/nanoshell.c
@@ -18,28 +18,3 @@ void LogMsg(const char* fmt, ...)
 	puts("");
 	va_end(lst);
 }
-
-void CoGetString(char* buffer, size_t szb)
-{
-	fgets(buffer, (int)szb, stdin);
-}
-
-
-void GetRidOfTrailingNewLine(char* buffer)
-{
-	char* b;
-	b = strrchr(buffer, '\n');
-	if (b) *b = 0;
-	b = strrchr(buffer, '\r');
-	if (b) *b = 0;
-}
-
-void ShellPrintMotd()
-{
-	LogMsg("Hello, this is NanoShell. How may I help you?");
-}
-
-void ShellExecuteCommand(const char* buffer)
-{
-	LogMsg("Got %s", buffer);
-}
